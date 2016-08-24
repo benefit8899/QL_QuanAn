ca6f3077fb0d2eb9f6b42b9ac8c9f1cb0520159a
benefit8899 <vanloi.infotech@gmail.com>
2016-08-24 17:00:29 +0700

    update project winsform

---
+++ QL_QuanAn/QL_QuanAn/CreateQRCode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace QL_QuanAn
+{
+    public class CreateQRCode
+    {
+        public string link = "vanloi.xyz/Maban=?";
+        Image codeImage;
+        
+        public CreateQRCode(string maban)
+        {
+            string linktable = link + maban;
+        }
+        public Image getImage()
+        {
+            return codeImage;
+        }
+    }
+}
