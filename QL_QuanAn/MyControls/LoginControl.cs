1fe0e947f837475369fcc5c24ff4208e0f4b37b6
benefit8899 <vanloi.infotech@gmail.com>
2016-08-25 09:37:00 +0700

    update md5 decrypt for login control

--- QL_QuanAn/MyControls/LoginControl.cs
+++ QL_QuanAn/MyControls/LoginControl.cs
@@ -15,10 +15,32 @@ namespace MyControls
         {
             InitializeComponent();
         }
-
+        public string GetMD5(string strkey)
+        {
+            string newkey = "";
+            byte[] array = System.Text.Encoding.UTF8.GetBytes(strkey);
+            MD5CryptoServiceProvider md5_provider = new MD5CryptoServiceProvider();
+            array = md5_provider.ComputeHash(array);
+            foreach (byte b in array)
+            {
+                newkey += b.ToString("X2");
+            }
+            return newkey;
+        }
         private void btnLogin_Click(object sender, EventArgs e)
         {
+            string strmk = GetMD5(txtPW.Text);
 
+            if (User.comparePW(strmk))
+            {
+                MessageBox.Show("Đăng nhập thành công!");
+                //Open form main
+            }
+            else
+             {
+                MessageBox.Show("Tên đăng nhập và mật khẩu không khớp!");
+                txtPW.Clear();
+             }
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
